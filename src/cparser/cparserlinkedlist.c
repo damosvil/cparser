29409bf0a1513a9410a67743d358740c31d5a605
iso9660 <dmv8086@gmail.com>
2019-12-09 18:39:38 +0100

    Implemented double linked list

---
+++ src/cparser/cparserlinkedlist.c
@@ -0,0 +1,146 @@
+/*
+ * cparserlinkedlist.c
+ *
+ *  Created on: 9 dic. 2019
+ *      Author: iso9660
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "cparserlinkedlist.h"
+
+
+struct cparserlinkedlist_s
+{
+	cparserlinkedlist_t *previous;
+	cparserlinkedlist_t *next;
+	void *item;
+};
+
+
+cparserlinkedlist_t *LinkedListNew(void *item)
+{
+	cparserlinkedlist_t *res = malloc(sizeof(cparserlinkedlist_t));
+
+	res->next = NULL;
+	res->previous = NULL;
+	res->item = item;
+
+	return res;
+}
+
+void LinkedListDelete(cparserlinkedlist_t *l)
+{
+	cparserlinkedlist_t *next;
+	cparserlinkedlist_t *prev;
+
+	// Check valid
+	if (l == NULL)
+		return;
+
+	// Store next and previous items
+	next = l->next;
+	prev = l->previous;
+
+	// Link
+	if (next && prev)
+	{
+		next->previous = prev;
+		prev->next = next;
+	}
+	else if (next)
+	{
+		next->previous = NULL;
+	}
+	else if (prev)
+	{
+		prev->next = NULL;
+	}
+	else
+	{
+		/* Do nothing */
+	}
+
+	// Free linked list item
+	free(l);
+}
+
+cparserlinkedlist_t *LinkedListFirst(cparserlinkedlist_t *l)
+{
+	while (l->previous)
+		l = l->previous;
+
+	return l;
+}
+
+cparserlinkedlist_t *LinkedListLast(cparserlinkedlist_t *l)
+{
+	while (l->next)
+		l = l->next;
+
+	return l;
+}
+
+cparserlinkedlist_t *LinkedListPrevious(cparserlinkedlist_t *l)
+{
+	return l->previous;
+}
+
+cparserlinkedlist_t *LinkedListNext(cparserlinkedlist_t *l)
+{
+	return l->next;
+}
+
+cparserlinkedlist_t *LinkedListInsertBefore(cparserlinkedlist_t *l, void *item)
+{
+	cparserlinkedlist_t *new;
+
+	if (l == NULL)
+		return NULL;
+
+	new = malloc(sizeof(cparserlinkedlist_t));
+	new->item = item;
+	new->previous = l->previous;
+	l->previous = new;
+
+	return new;
+}
+
+cparserlinkedlist_t *LinkedListInsertAfter(cparserlinkedlist_t *l, void *item)
+{
+	cparserlinkedlist_t *new;
+
+	if (l == NULL)
+		return NULL;
+
+	new = malloc(sizeof(cparserlinkedlist_t));
+	new->item = item;
+	new->next = l->next;
+	l->next = new;
+
+	return new;
+}
+
+void *LinkedListGetItem(cparserlinkedlist_t *l)
+{
+	if (l == NULL)
+		return NULL;
+
+	return l->item;
+}
+
+void *LinkedListUpdateItem(cparserlinkedlist_t *l, void *item)
+{
+	void *res;
+
+	if (l == NULL)
+		return NULL;
+
+	res = l->item;
+	l->item = item;
+
+	return res;
+}
+
+
--- src/cparser/cparserlinkedlist.h
+++ src/cparser/cparserlinkedlist.h
@@ -9,7 +9,20 @@
 #define CPARSER_CPARSERLINKEDLIST_H_
 
 
+struct cparserlinkedlist_s;
+typedef struct cparserlinkedlist_s cparserlinkedlist_t;
 
+
+cparserlinkedlist_t *LinkedListNew(void *item);
+void LinkedListDelete(cparserlinkedlist_t *l);
+cparserlinkedlist_t *LinkedListFirst(cparserlinkedlist_t *l);
+cparserlinkedlist_t *LinkedListLast(cparserlinkedlist_t *l);
+cparserlinkedlist_t *LinkedListPrevious(cparserlinkedlist_t *l);
+cparserlinkedlist_t *LinkedListNext(cparserlinkedlist_t *l);
+cparserlinkedlist_t *LinkedListInsertBefore(cparserlinkedlist_t *l, void *item);
+cparserlinkedlist_t *LinkedListInsertAfter(cparserlinkedlist_t *l, void *item);
+void *LinkedListGetItem(cparserlinkedlist_t *l);
+void *LinkedListUpdateItem(cparserlinkedlist_t *l, void *item);
 
 
 #endif /* CPARSER_CPARSERLINKEDLIST_H_ */
